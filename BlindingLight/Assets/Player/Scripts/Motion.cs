6fb0ac6c5a8290b60c951b644b15362eb2f26ef9
lightkingdom17 <bhavyarajput17@gmail.com>
2022-03-08 10:25:52 -0800

    cont. work on rotation
    try using fps logic

---
+++ BlindingLight/Assets/Player/Scripts/Motion.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion : MonoBehaviour
+{
+
+    public float velocity = 5f;
+    public float sensitivity = 100f;
+    public Rigidbody player;
+    public Transform play;
+    public Camera cam;
+    Vector3 movement;
+    Vector3 mousePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.z = Input.GetAxis("Vertical");
+        mousePosition = Input.mousePosition;
+        movmentCalc();
+        rotationCalc();
+        
+    }
+    //moves the character in acordance to the key inputs
+    void movmentCalc(){
+        transform.Translate(movement * velocity * Time.deltaTime);
+    }
+
+    void rotationCalc(){
+        // float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        
+        // play.Rotate(Vector3.up * mouseX);
+
+        float difference = mousePosition - player.position;
+    }
+}
