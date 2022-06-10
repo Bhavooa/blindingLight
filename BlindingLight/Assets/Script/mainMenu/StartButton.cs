982f1c8338071d0b563228ccccd83375cb5f94f3
lightkingdom17 <bhavyarajput17@gmail.com>
2022-06-10 10:34:50 -0700

    Actually finished
    Added resolution adjustment

--- BlindingLight/Assets/Script/mainMenu/StartButton.cs
+++ BlindingLight/Assets/Script/mainMenu/StartButton.cs
@@ -7,6 +7,8 @@ public class StartButton : MonoBehaviour
 {
     //sets at the first frame
     void Start(){
+        //sets resoloutiion
+        Screen.SetResolution(2560,1600, true, 0);
         //show mouse
         Cursor.lockState = CursorLockMode.None;
     }
